be6fde5d410ec23ebfd98ba6bb547eb0f8b9c2b8
ingfrf <ingfrf@gmail.com>
2022-12-02 00:23:09 +0100

    streams

--- src/main/java/com/enmivida/info/controller/MovieInfoController.java
+++ src/main/java/com/enmivida/info/controller/MovieInfoController.java
@@ -4,10 +4,12 @@ import com.enmivida.info.domain.MovieInfo;
 import com.enmivida.info.service.MovieInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 import javax.validation.Valid;
 
@@ -17,12 +19,23 @@ import javax.validation.Valid;
 public class MovieInfoController {
 
     private final MovieInfoService service;
+    // replay va a reenviar todos los updates a los suscriptores
+    // private Sinks.Many<MovieInfo> movieInfoSinks = Sinks.many().replay().all();
+    // los suscriptores recibiran el último al suscribirse
+    private Sinks.Many<MovieInfo> movieInfoSink = Sinks.many().replay().latest();
 
     @PostMapping("/movieinfos")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<MovieInfo> addMovieInfo(@RequestBody @Valid MovieInfo movieInfo) {
         return service.addMovieInfo(movieInfo)
-                .log();
+                .doOnNext(savedMovieInfo -> movieInfoSink.tryEmitNext(savedMovieInfo))
+                ;
+    }
+
+    // un entrypoint para el suscriptor
+    @GetMapping(value = "/movieinfos/stream", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<MovieInfo> getAllMovieInfo() {
+       return movieInfoSink.asFlux();
     }
 
     @GetMapping("/movieinfos")
